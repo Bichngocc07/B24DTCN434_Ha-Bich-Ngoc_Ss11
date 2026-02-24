fc91a49df3ebf621cd51cac75c99df6b9d76ff6e
Bichngocc07 <ngoc01072006@gmail.com>
2026-02-24 09:21:10 +0700

    Add files via upload

---
+++ B24DTCN434_Ha Bich Ngoc_Ss11/src/AbstractlassDrink.java
@@ -0,0 +1,11 @@
+public class AbstractlassDrink {
+    private String id;
+    private String name;
+    private double price;
+
+    public AbstractlassDrink(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
